15f6059ffb8df2b6dc53127fced2494e70a71ec4
Hari <a8b10c12@gmail.com>
2025-07-24 20:13:45 +0530

    Update

--- streamlit_face_auth.py
+++ streamlit_face_auth.py
@@ -12,8 +12,8 @@ from supabase import create_client , Client
 
 load_dotenv()
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_URL = st.secrets.get("SUPABASE_URL", os.getenv("SUPABASE_URL"))
+SUPABASE_KEY = st.secrets.get("SUPABASE_KEY", os.getenv("SUPABASE_KEY"))
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
